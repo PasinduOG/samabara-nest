7c5d282863aa88960285a780ecc7c9c11c8ca69b
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-06-23 00:16:01 +0530

    feat: add Prisma module and service for database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { FamilyModule } from './family/family.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, FamilyModule, TransactionModule]
+  imports: [UserModule, FamilyModule, TransactionModule, PrismaModule]
 })
 export class AppModule {}
